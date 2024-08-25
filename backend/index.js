619311ab74950e553fcb9cef3a9d791256d427ea
shaif-codes <91661068+shaif-codes@users.noreply.github.com>
2024-08-25 18:46:15 +0530

    chore: Update CORS configuration for backend API

--- backend/index.js
+++ backend/index.js
@@ -16,12 +16,12 @@ require('./cron/evaluateTests'); // Import cron jobs
 const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());  // Add this line
-// app.use(cors({
-//     origin: process.env.CLIENT_URL,
-//     credentials: true
-// }));
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}));
 
-app.use(cors());
+// app.use(cors());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI).then(() => {
